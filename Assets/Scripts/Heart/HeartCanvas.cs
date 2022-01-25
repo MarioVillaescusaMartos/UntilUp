01819c4dc38bf935d76fcd442009692db884c2f5
MarioVillaescusaMartos <villaescusam.mario@gmail.com>
2022-01-25 12:32:20 +0100

    ADD heartscore, heartmanager

--- Assets/Scripts/Heart/HeartCanvas.cs
+++ Assets/Scripts/Heart/HeartCanvas.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreCanvas : MonoBehaviour
+public class HeartCanvas : MonoBehaviour
 {
     [SerializeField]
-    private Text scoreDisplay;
+    private Text heartDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreDisplay.text = "Score: " + ScoreManager.score.ToString();
+        heartDisplay.text = "Heart: " + HeartManager.heart.ToString();
     }
 }
